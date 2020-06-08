4ecfe95db2217d4dc89e9ea32a8f4b71954d18db
drinkjava2 <yong9981@gmail.com>
2020-06-08 08:39:51 -0700

    snake almost added, will add big eye

--- core/src/main/java/com/github/drinkjava2/frog/Env.java
+++ core/src/main/java/com/github/drinkjava2/frog/Env.java
@@ -1,5 +1,8 @@
 package com.github.drinkjava2.frog;
 
+import static com.github.drinkjava2.frog.Env.ENV_HEIGHT;
+import static com.github.drinkjava2.frog.Env.ENV_WIDTH;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -38,8 +41,6 @@ public class Env extends JPanel {
 
 	public static final int SCREEN = 1; // 分几屏测完
 
-	public static final int FROG_PER_SCREEN = FROG_EGG_QTY * FROG_PER_EGG / SCREEN; // 每屏上显示几个青蛙，这个数值由上面三个参数计算得来
-
 	/** Frog's brain size is a 3D array of Cell */ // 脑空间是个三维Cell数组，为节约内存，仅在用到数组元素时才去初始化这维，按需分配内存
 	public static final int FROG_BRAIN_XSIZE = 1000; // frog的脑在X方向长度
 	public static final int FROG_BRAIN_YSIZE = 1000; // frog的脑在Y方向长度
@@ -67,7 +68,25 @@ public class Env extends JPanel {
 	public static final int FOOD_QTY = 1500; // 食物数量, 可调
 	public static boolean FOOD_CAN_MOVE = false; // 食物是否可以移动，眼花
 
+	// =========以下是与蛇相关的常量和变量===========
+
+	public static final boolean DELETE_SNAKE_EGGS = true;// 每次运行是否先删除保存的蛇蛋
+
+	public static boolean SNAKE_MODE = true; // 是否加小蛇加进来吃青蛙?
+
+	public static final int SNAKE_EGG_QTY = 5; // 每轮下n个蛇蛋，可调，只有最优秀的前n个蛇们才允许下蛋
+
+	public static final int SNAKE_PER_EGG = 4; // 每个蛇蛋可以孵出几个蛇
+
 	// 以下是程序内部变量，不要手工修改它们
+	public static final int FROG_PER_SCREEN = FROG_EGG_QTY * FROG_PER_EGG / SCREEN; // 每屏显示几个青蛙，这个数值由其它常量计算得来
+
+	public static int current_screen = 0;
+
+	public static int food_ated = 0; // 用来统计总共多少个食物被青蛙吃掉
+
+	public static int frog_ated = 0; // 用来统计总共多少个食物被青蛙吃掉
+
 	public static boolean pause = false; // 暂停按钮按下将暂停测试
 
 	public static int[][] bricks = new int[ENV_WIDTH][ENV_HEIGHT];// 组成环境的材料，见Material.java
@@ -78,17 +97,9 @@ public class Env extends JPanel {
 
 	public static EnvObject[] things = new EnvObject[] { new Food() };// 所有外界物体，如食物、字母测试工具都放在这个things里面
 
-	// =========以下是与蛇相关的常量和变量===========
+	public static final int TOTAL_SNAKE_QTY = SNAKE_EGG_QTY * SNAKE_PER_EGG; // 蛇的总数
 
-	public static final boolean DELETE_SNAKE_EGGS = true;// 每次运行是否先删除保存的蛇蛋
-
-	public static boolean SNAKE_MODE = true; // 是否加小蛇加进来吃青蛙?
-
-	public static final int SNAKE_EGG_QTY = 10; // 每轮下n个蛇蛋，可调，只有最优秀的前n个蛇们才允许下蛋
-
-	public static final int SNAKE_PER_EGG = 4; // 每个蛇蛋可以孵出几个蛇
-
-	public static final int SNAKE_PER_SCREEN = SNAKE_EGG_QTY * SNAKE_PER_EGG / SCREEN; // 每屏上显示几个蛇，这个数值由上面参数计算得来
+	public static final int SNAKE_PER_SCREEN = TOTAL_SNAKE_QTY / SCREEN; // 每屏显示几个蛇，这个数值由其它常量计算得来
 
 	public static List<Snake> snakes = new ArrayList<>(); // 这里存放所有待测的蛇，可能分几次测完，由FROG_PER_SCREEN大小来决定
 
@@ -121,36 +132,54 @@ public class Env extends JPanel {
 		return f.x < 20 || f.y < 20 || f.x > (Env.ENV_WIDTH - 20) || f.y > (Env.ENV_HEIGHT - 20);
 	}
 
-	public static boolean foundAnyThing(int x, int y) {// 如果指定点看到任意东西或超出边界
+	public static boolean foundAnyThing(int x, int y) {// 如果指定点看到任意东西或超出边界，返回true
 		return x < 0 || y < 0 || x >= ENV_WIDTH || y >= ENV_HEIGHT || Env.bricks[x][y] >= Material.VISIBLE;
 	}
 
-	public static boolean foundFrog(int x, int y) {// TODO:优化寻找青蛙的速度，不要用循环 如果指定点看到青蛙或超出边界，返回true
-		if (x < 0 || y < 0 || x >= ENV_WIDTH || y >= ENV_HEIGHT)
-			return true;
-		for (Frog f : frogs)
-			if (f.alive && f.x == x && f.y == y) {
-				return true;
-			}
-		return false;
-	}
-
 	public static boolean foundAndAteFood(int x, int y) {// 如果x,y有食物，将其清0，返回true
-		if (insideEnv(x, y) && Env.bricks[x][y] >= Material.FOOD && Env.bricks[x][y] <= Material.FLY4) {
-			Food.food_ated++;
-			bricks[x][y] = 0;
+		if (insideEnv(x, y) && (Env.bricks[x][y] & Material.ANY_FOOD) > 0) {
+			Env.food_ated++;
+			clearMaterial(x, y, Material.ANY_FOOD);// 清空任意食物
 			return true;
 		}
 		return false;
 	}
 
+	public static boolean foundFrog(int x, int y) {// 如果指定点看到青蛙或超出边界，返回true
+		if (x < 0 || y < 0 || x >= ENV_WIDTH || y >= ENV_HEIGHT)
+			return false;// 如果出界返回true
+		int frogNo = Env.bricks[x][y] & Material.FROG_TAG;
+		if (frogNo > 0) {
+			Frog f = frogs.get(frogNo - 1);
+			if (f.alive)
+				return true;
+		}
+		return false;
+	}
+
 	public static boolean foundAndAteFrog(int x, int y) {// TODO:优化寻找青蛙的速度，不要用循环 如果x,y有青蛙，将其杀死，返回true
-		for (Frog f : frogs)
-			if (f.alive && f.x == x && f.y == y) {
+		if (x < 0 || y < 0 || x >= ENV_WIDTH || y >= ENV_HEIGHT)
+			return false;// 如果出界返回false;
+		int frogNo = Env.bricks[x][y] & Material.FROG_TAG;
+		if (frogNo > 0) {
+			Frog f = frogs.get(frogNo - 1);
+			if (f.alive) {
+				Env.frog_ated++;
 				f.alive = false;
 				return true;
 			}
+		}
 		return false;
+	}
+
+	public static void setMaterial(int x, int y, int material) {
+		if (Env.insideEnv(x, y))
+			Env.bricks[x][y] = Env.bricks[x][y] | material;
+	}
+
+	public static void clearMaterial(int x, int y, int material) {
+		if (Env.insideEnv(x, y))
+			Env.bricks[x][y] = Env.bricks[x][y] & ~material;
 	}
 
 	private void rebuildFrogs() {// 根据蛙蛋重新孵化出蛙
@@ -165,7 +194,9 @@ public class Env extends JPanel {
 			}
 			for (int j = 0; j < loop; j++) {
 				Egg zygote = new Egg(frog_eggs.get(i), frog_eggs.get(RandomUtils.nextInt(frog_eggs.size())));
-				frogs.add(new Frog(RandomUtils.nextInt(ENV_WIDTH), RandomUtils.nextInt(ENV_HEIGHT), zygote));
+				Frog f = new Frog(RandomUtils.nextInt(ENV_WIDTH), RandomUtils.nextInt(ENV_HEIGHT), zygote);
+				frogs.add(f);
+				f.no = frogs.size();
 			}
 		}
 	}
@@ -174,7 +205,7 @@ public class Env extends JPanel {
 		snakes.clear();
 		for (int i = 0; i < snake_eggs.size(); i++) {// 创建蛇，每个蛋生成n个蛇，并随机取一个别的蛋作为精子
 			int loop = SNAKE_PER_EGG;
-			if (snake_eggs.size() > 20) { // 如果数量多，进行一些优化，让排名靠前的Egg多孵出青蛙
+			if (snake_eggs.size() > 20) { // 如果数量多，进行一些优化，让排名靠前的Egg多孵出蛇
 				if (i < SNAKE_PER_EGG)// 0,1,2,3
 					loop = SNAKE_PER_EGG + 1;
 				if (i >= (snake_eggs.size() - SNAKE_PER_EGG))
@@ -209,13 +240,17 @@ public class Env extends JPanel {
 		format100.setMaximumFractionDigits(2);
 	}
 
-	private String foodFoundCountText() {// 统计找食数等
+	private String foodAtedCount() {// 统计吃食总数等
 		int maxFound = 0;
 		for (Frog f : frogs)
 			if (f.ateFood > maxFound)
 				maxFound = f.ateFood;
-		return new StringBuilder("找食率:").append(format100.format(Food.food_ated * 1.00 / FOOD_QTY)).append(", 平均: ")
-				.append(Food.food_ated * 1.0f / FROG_PER_SCREEN).append("，最多:").append(maxFound).toString();
+		return new StringBuilder("吃食率:").append(format100.format(Env.food_ated * 1.00 / FOOD_QTY)).append(", 平均: ")
+				.append(Env.food_ated * 1.0f / FROG_PER_SCREEN).append("，最多:").append(maxFound).toString();
+	}
+
+	private String frogAtedCount() {// 统计食蛙总数
+		return new StringBuilder("吃蛙率:").append(format100.format(Env.frog_ated * 1.00 / TOTAL_SNAKE_QTY)).toString();
 	}
 
 	public static void checkIfPause(Frog f) {
@@ -249,40 +284,42 @@ public class Env extends JPanel {
 			rebuildFrogs();
 			if (SNAKE_MODE)
 				rebuildSnakes();
-			for (int screen = 0; screen < SCREEN; screen++) {// 分屏测试，每屏FROG_PER_SCREEN个蛙
+			for (current_screen = 0; current_screen < SCREEN; current_screen++) {// 分屏测试，每屏FROG_PER_SCREEN个蛙
+				Env.food_ated = 0; // 先清0吃食物数
+				Env.frog_ated = 0;// 先清0吃蛙数
 				time0 = System.currentTimeMillis();
 				for (EnvObject thing : things) // 创建食物、陷阱等物体
 					thing.build();
 				boolean allDead = false;
-				Frog firstFrog = frogs.get(screen * FROG_PER_SCREEN);
+				Frog firstFrog = frogs.get(current_screen * FROG_PER_SCREEN);
 				Snake firstSnake = null;
 
 				for (int j = 0; j < FROG_PER_SCREEN; j++) {
-					Frog f = frogs.get(screen * FROG_PER_SCREEN + j);
+					Frog f = frogs.get(current_screen * FROG_PER_SCREEN + j);
 					f.initFrog(); // 初始化器官延迟到这一步，是因为脑细胞太占内存，而且当前屏测完后会清空
 				}
 				if (SNAKE_MODE && !snakes.isEmpty()) {
-					firstSnake = snakes.get(screen * SNAKE_PER_SCREEN);
+					firstSnake = snakes.get(current_screen * SNAKE_PER_SCREEN);
 					for (int j = 0; j < SNAKE_PER_SCREEN; j++) {
-						Snake s = snakes.get(screen * SNAKE_PER_SCREEN + j);
+						Snake s = snakes.get(current_screen * SNAKE_PER_SCREEN + j);
 						s.initFrog(); // 初始化器官延迟到这一步，是因为脑细胞太占内存，而且当前屏测完后会清空
 					}
 				}
 				Frog showFrog = Application.selectFrog ? firstFrog : firstSnake;
 				for (step = 0; step < STEPS_PER_ROUND; step++) {
 					for (EnvObject thing : things)// 调用食物、陷阱等物体的动作
-						thing.active(screen);
+						thing.active(current_screen);
 					if (allDead)
 						break; // 青蛙全死光了就直接跳到下一轮,以节省时间
 					allDead = true;
 					for (int j = 0; j < FROG_PER_SCREEN; j++) {
-						Frog f = frogs.get(screen * FROG_PER_SCREEN + j);
+						Frog f = frogs.get(current_screen * FROG_PER_SCREEN + j);
 						if (f.active(this))// 调用青蛙的Active方法，并返回是否还活着
 							allDead = false;
 					}
 					if (SNAKE_MODE && !snakes.isEmpty())
 						for (int j = 0; j < SNAKE_PER_SCREEN; j++) {
-							Snake s = snakes.get(screen * SNAKE_PER_SCREEN + j);
+							Snake s = snakes.get(current_screen * SNAKE_PER_SCREEN + j);
 							s.active(this);// snake唯一作用就是吃小蛇
 						}
 
@@ -298,12 +335,12 @@ public class Env extends JPanel {
 					g.setColor(Color.BLACK);
 					drawWorld(g);// 画整个虚拟环境
 					for (int j = 0; j < FROG_PER_SCREEN; j++) { // 显示青蛙
-						Frog f = frogs.get(screen * FROG_PER_SCREEN + j);
+						Frog f = frogs.get(current_screen * FROG_PER_SCREEN + j);
 						f.show(g);
 					}
 					if (SNAKE_MODE && !snakes.isEmpty())
 						for (int j = 0; j < SNAKE_PER_SCREEN; j++) { // 显示蛇
-							Snake s = snakes.get(screen * SNAKE_PER_SCREEN + j);
+							Snake s = snakes.get(current_screen * SNAKE_PER_SCREEN + j);
 							s.show(g);
 						}
 
@@ -322,15 +359,24 @@ public class Env extends JPanel {
 					Application.brainPic.drawBrainPicture(showFrog);
 				checkIfPause(firstFrog);
 				for (int j = 0; j < FROG_PER_SCREEN; j++) {
-					Frog f = frogs.get(screen * FROG_PER_SCREEN + j);
+					Frog f = frogs.get(current_screen * FROG_PER_SCREEN + j);
 					f.cells = null; // 清空frog脑细胞所占用的内存
 				}
-				Application.mainFrame
-						.setTitle(new StringBuilder("Round: ").append(round).append(", screen:").append(screen)
-								.append(", speed:").append(Env.SHOW_SPEED).append(", ").append(foodFoundCountText())
-								.append(", 用时: ").append(System.currentTimeMillis() - time0).append("ms").toString());
-				for (EnvObject thing : things)// 去除食物、陷阱等物体
-					thing.destory();
+				StringBuilder sb = new StringBuilder("Round: ");
+				sb.append(round).append(", screen:").append(current_screen).append(", speed:").append(Env.SHOW_SPEED)
+						.append(", ").append(", 用时: ").append(System.currentTimeMillis() - time0).append("ms, ");
+				if (SNAKE_MODE && !Application.selectFrog)
+					sb.append(frogAtedCount());
+				else
+					sb.append(foodAtedCount());
+
+				Application.mainFrame.setTitle(sb.toString());
+//				for (EnvObject thing : things)// 去除食物、陷阱等物体
+//					thing.destory();
+				for (int i = 0; i < ENV_WIDTH; i++) {// 清除食物
+					for (int j = 0; j < ENV_HEIGHT; j++)
+						bricks[i][j]=0;
+				}
 			}
 			round++;
 			FrogEggTool.layEggs();
